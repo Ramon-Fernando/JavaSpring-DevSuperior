d7a7f96689b873c734834e297a9287e031ced2bc
Ramon <ramon.fernando98@gmail.com>
2025-06-18 19:01:41 -0300

    Order entity done

---
+++ Java Spring Professional/01 - Componentes e Injeção de Dependência/Desafio/src/main/java/com/ramon/Desafio/entities/Order.java
@@ -0,0 +1,54 @@
+package com.ramon.Desafio.entities;
+
+import java.util.Objects;
+
+public class Order {
+    private Integer code;
+    private Double basic;
+    private Double discount;
+
+    public Order(){
+    }
+
+    public Order(Integer code, Double basic, Double discount) {
+        this.code = code;
+        this.basic = basic;
+        this.discount = discount;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Double getBasic() {
+        return basic;
+    }
+
+    public void setBasic(Double basic) {
+        this.basic = basic;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(code, order.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(code);
+    }
+}
