6d5ed54372634aff7b9cbf704e60cebc862c3937
Ramon <ramon.fernando98@gmail.com>
2025-06-18 18:43:51 -0300

    Spring project created

---
+++ Java Spring Professional/01 - Componentes e Injeção de Dependência/Desafio/src/test/java/com/ramon/Desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.Desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
