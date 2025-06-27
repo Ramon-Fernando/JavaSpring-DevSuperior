247a378094e643ba37896533603dd53a1bae89d8
Ramon <ramon.fernando98@gmail.com>
2025-06-27 05:07:59 -0300

    Entity 'Bloco' created

--- Java Spring Professional/02 - Modelo de Domínio e ORM/Desafio/src/main/java/com/ramon/evento/entities/Participante.java
+++ Java Spring Professional/02 - Modelo de Domínio e ORM/Desafio/src/main/java/com/ramon/evento/entities/Participante.java
@@ -2,6 +2,8 @@ package com.ramon.evento.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_participante")
 
@@ -51,5 +53,17 @@ public class Participante {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Participante that = (Participante) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
