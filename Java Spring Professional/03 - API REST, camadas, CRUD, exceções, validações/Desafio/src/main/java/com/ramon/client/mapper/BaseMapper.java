78b6a0812be17ad44339725f91b4d4268c098e08
Ramon <ramon.fernando98@gmail.com>
2025-07-08 05:42:38 -0300

    BaseMapper updated

--- Java Spring Professional/03 - API REST, camadas, CRUD, exceções, validações/Desafio/src/main/java/com/ramon/client/mapper/BaseMapper.java
+++ Java Spring Professional/03 - API REST, camadas, CRUD, exceções, validações/Desafio/src/main/java/com/ramon/client/mapper/BaseMapper.java
@@ -1,6 +1,10 @@
 package com.ramon.client.mapper;
 
+import org.mapstruct.MappingTarget;
+
 public interface BaseMapper<DTO, Entity> {
     DTO toDTO(Entity entity);
     Entity toEntity(DTO dto);
+
+    void updateEntityFromDTO(DTO dto, @MappingTarget Entity entity);
 }
