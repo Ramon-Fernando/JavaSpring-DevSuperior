e07fc92f1adb8cac2bf4126b6de1152b9493910f
Ramon <ramon.fernando98@gmail.com>
2025-07-08 06:11:44 -0300

    Mapper updated

--- Java Spring Professional/03 - API REST, camadas, CRUD, exceções, validações/Desafio/src/main/java/com/ramon/client/mapper/BaseMapper.java
+++ Java Spring Professional/03 - API REST, camadas, CRUD, exceções, validações/Desafio/src/main/java/com/ramon/client/mapper/BaseMapper.java
@@ -1,10 +1,9 @@
 package com.ramon.client.mapper;
 
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 public interface BaseMapper<DTO, Entity> {
     DTO toDTO(Entity entity);
     Entity toEntity(DTO dto);
-
-    void updateEntityFromDTO(DTO dto, @MappingTarget Entity entity);
 }
--- Java Spring Professional/03 - API REST, camadas, CRUD, exceções, validações/Desafio/src/main/java/com/ramon/client/mapper/ClientMapper.java
+++ Java Spring Professional/03 - API REST, camadas, CRUD, exceções, validações/Desafio/src/main/java/com/ramon/client/mapper/ClientMapper.java
@@ -3,7 +3,12 @@ package com.ramon.client.mapper;
 import com.ramon.client.dto.ClientDTO;
 import com.ramon.client.entities.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper extends BaseMapper<ClientDTO, Client> {
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDTO(ClientDTO dto, @MappingTarget Client entity);
 }
