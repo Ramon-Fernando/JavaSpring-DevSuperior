9dc5a46c92f04e7cec9d54ea8e4e5ef472a18b85
Ramon <ramon.fernando98@gmail.com>
2025-07-20 13:15:37 -0300

    showSumary service done

--- Java Spring Professional/04 - JPA, consultas SQL e JPQL/Desafio/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ Java Spring Professional/04 - JPA, consultas SQL e JPQL/Desafio/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SalesReportDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -12,12 +16,24 @@ import com.devsuperior.dsmeta.repositories.SaleRepository;
 @Service
 public class SaleService {
 
-	@Autowired
 	private SaleRepository repository;
+
+	public SaleService(SaleRepository repository) {
+		this.repository = repository;
+	}
 	
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	public Page<SalesReportDTO> showSummary(String startDate, String endDate, String sellerName, Pageable pageable) {
+		LocalDate end = endDate.isBlank() ? LocalDate.now() : LocalDate.parse(endDate);
+		LocalDate start = startDate.isBlank() ? end.minusYears(1L) : LocalDate.parse(startDate);
+
+		return repository.showSummary(start, end, sellerName, pageable);
+	}
+
+
 }
