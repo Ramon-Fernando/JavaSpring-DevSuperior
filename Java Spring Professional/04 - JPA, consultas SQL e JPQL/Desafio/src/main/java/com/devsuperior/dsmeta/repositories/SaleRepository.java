0318d367109f65ff9e2db97469ff76b2f6a5691b
Ramon <ramon.fernando98@gmail.com>
2025-07-20 12:45:25 -0300

    showSummary method created in repository

--- Java Spring Professional/04 - JPA, consultas SQL e JPQL/Desafio/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ Java Spring Professional/04 - JPA, consultas SQL e JPQL/Desafio/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,10 +1,23 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SalesReportDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SalesReportDTO(obj.id, obj.date, obj.amount, obj.seller.name) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :startDate AND :endDate "
+            + "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :sellerName,'%'))")
+    Page<SalesReportDTO> showSummary (LocalDate startDate, LocalDate endDate, String sellerName, Pageable pageable);
+
 
 
 
