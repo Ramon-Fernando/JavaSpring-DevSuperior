2c8804e503b3bd847fc7aa3c5d91dbef65ef32b8
Ramon <ramon.fernando98@gmail.com>
2025-07-20 14:24:40 -0300

    showSummary method in service done

--- Java Spring Professional/04 - JPA, consultas SQL e JPQL/Desafio/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ Java Spring Professional/04 - JPA, consultas SQL e JPQL/Desafio/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import com.devsuperior.dsmeta.dto.SalesReportDTO;
+import com.devsuperior.dsmeta.dto.SalesSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -35,5 +36,11 @@ public class SaleService {
 		return repository.showReport(start, end, sellerName, pageable);
 	}
 
+	public Page<SalesSummaryDTO> showSummary(String startDate, String endDate, String sellerName, Pageable pageable) {
+		LocalDate end = endDate.isBlank() ? LocalDate.now() : LocalDate.parse(endDate);
+		LocalDate start = startDate.isBlank() ? end.minusYears(1L) : LocalDate.parse(startDate);
+
+		return repository.showSummary(start, end, sellerName, pageable);
+	}
 
 }
